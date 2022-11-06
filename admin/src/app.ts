d8d66d1a3a737c2d2d9332ceed81294b7d4a638f
pankaj shrivastava <shrivastava74795@gmail.com>
2022-11-06 11:04:33 +0530

    admin api

---
+++ admin/src/app.ts
@@ -0,0 +1,33 @@
+import * as express from 'express';
+import {Request,Response} from 'express';
+import * as cors from 'cors';
+import {createConnection} from 'typeorm';
+import { Product } from './entity/product';
+
+
+createConnection().then(db => {
+    const productRepository =  db.getRepository(Product);
+    const app = express();
+
+    app.use(cors({
+        origin:['http://localhost:3000','http://localhost:8080','http://localhost:42000']
+    }));
+    
+    app.use(express.json());
+    
+    app.get('/api/products',async(req:Request,res:Response) => {
+        const products = await productRepository.find();
+
+        res.json(products)
+    })
+    
+    app.post('/api/products',async(req:Request,res:Response) => {
+        const product = await productRepository.create(req.body);
+        const result = await productRepository.save(product);
+        return res.send(result);
+    })
+    console.log('listening to port 8000');
+    app.listen(8000)
+})
+
+
